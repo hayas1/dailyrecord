fa5d058e6733534506080c4933a5e2eaca18e731
hayas1 <h4ystack@gmail.com>
2023-04-02 13:32:07 +0000

    use gloo::console for logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use dailyrecord::component::route::Router;
 
 fn main() {
-    wasm_logger::init(wasm_logger::Config::default());
-    log::debug!("Hello, this is {} (version {})", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    gloo::console::log!(format!("hello, this is {} (version {})", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")));
 
     yew::Renderer::<Router>::new().render();
 }
