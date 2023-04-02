c8dfae2b8bc96d8d548bf4c41f5f73ed28b41dc3
hayas1 <h4ystack@gmail.com>
2023-04-02 08:54:31 +0000

    version

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use dailyrecord::component::route::Router;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
-    log::debug!("Hello, this is {}", env!("CARGO_PKG_NAME"));
+    log::debug!("Hello, this is {} (version {})", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
 
     yew::Renderer::<Router>::new().render();
 }
